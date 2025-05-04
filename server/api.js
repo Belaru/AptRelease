d74bb4c11228eefbfc06ee28b230396b5a0339fd
Belaru <nactiabond.ota@gmail.com>
2025-05-04 14:26:44 -0400

    Debug CORS

--- server/api.js
+++ server/api.js
@@ -23,6 +23,15 @@ __filename || typeof require !== 'undefined' && require('url').fileURLToPath || 
 const app = express();
 
 app.use(cors());
+app.options('*', cors());
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();  
+};
+console.log('Cors Enabled');
+app.use(allowCrossDomain);
 app.use(express.static(path.join(__dirname, 'client/build')));
 // Add middleware to serve static files
 app.use(
